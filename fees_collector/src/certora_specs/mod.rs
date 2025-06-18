eaeab0079f798f83a570479f4a2f01d6fbade816
Abdullah Alsigar <abdullah.alsigar@gmail.com>
2025-06-18 20:32:33 +0300

    mod

--- fees_collector/src/certora_specs/mod.rs
+++ fees_collector/src/certora_specs/mod.rs
@@ -4,7 +4,11 @@ pub mod fees_collector_mutant_1_verified;
 pub mod fees_collector_mutant_2_verified;
 pub mod access_control_set_role_address_verified;
 pub mod access_control_set_role_addresses_verified;
-pub mod access_control_emergency_mode_setter_getter_verified;
+pub mod access_control_transfer_ownership_rules;
+pub mod access_control_role_symbol_rules;
+pub mod access_control_authorization_simple_rules;
+pub mod access_control_emergency_mode_rules;
+pub mod access_control_storage_edge_cases_rules;
 pub mod util;
 
 use access_control::access::AccessControl;
